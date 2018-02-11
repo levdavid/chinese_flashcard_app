1b12de712942ca259ec050ac7dfcee81c25f7fed
root <root@localhost.localdomain>
2018-02-11 14:46:16 -0500

    fix dataProvider

--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -1,17 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the DataProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class DataProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello DataProvider Provider');
-  }
+data: any;
 
+constructor(public http: HttpClient) {
+    console.log('Hello DataProvider Provider');
+    }
+
+    load(){
+        if(this.data){
+            return Promise.resolve(this.data);
+        }
+
+        return new Promise(resolve=> {
+            this.http.get('assets/data/questions.json').map(res => res.json()).subscribe(data => {
+                this.data = data.questions;
+                resolve(this.data);
+            });
+        });
+    }
 }
